523f848b0af2c2f255ad01630b4f63bafe85b513
sanjana shah <sanjanashah0148@gmail.com>
2026-07-07 10:24:21 +0000

    datatype

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,24 @@
+let score="33abc"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//"33"=>33
+//"33abc"=>NaN
+//truue=>1;false=>0
+
+let isLoggedIn="Sanjana"
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1=>true;0=>false
+//""=>false
+//"sanjana"=>true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
