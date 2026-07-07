dc2ac17ece39d24195c642f0e104f4ffcc02d86b
sanjana shah <sanjanashah0148@gmail.com>
2026-07-07 09:49:54 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all JS code as newer version
+//alert("3+3")//we are using node js not browser
+console.log(3+3);
+console.log("Sanjana");
+
+let name="sanjana";
+let age=18;
+let isLoggesIn=false;
+let state;
+// number=> 2 to power 53
+//bigint
+//string
+//boolean=>true/false
+//null-standalone value
+//undefined
+
+
+//object
+
+console.log(typeof"sanjana");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
