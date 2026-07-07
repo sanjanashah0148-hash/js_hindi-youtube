e49b1790ff9f11de0aede1c08b1a3d214426a66b
sanjana shah <sanjanashah0148@gmail.com>
2026-07-07 09:51:14 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 14453
+let accountEmail="sanjanashah0148@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountstate;
+// accountId=2 //not allowed
+
+accountEmail="hdc@gmail.com"
+accountPassword="1234567"
+accountCity="Prayagraj"
+
+/*
+prefer not to use var
+becoz of issue in it block scope and functional scope
+*/
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword.accountCity,accountstate])
