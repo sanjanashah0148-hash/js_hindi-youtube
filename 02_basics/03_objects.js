50cf7f4b9384abe600223217de6f80e9fa21c671
sanjana shah <sanjanashah0148@gmail.com>
2026-07-10 17:04:59 +0000

    object pura huaaa!!hahaah

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,7 +1,37 @@
 //singleton
 
 //obejct literal
-object.create
+//object.create
+
+const mySym=Symbol("key1")//to declare a symbol
 const JsUser={
-    name:"Sanjana"
+    name:"Sanjana",
+    "FullName":"Sanjana Shah",
+    age:20,
+    [mySym]:"mykey1",
+    location:"Prayagraj",
+    email:"sanjana.20249026@mnnit.ac.in",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"]
 }
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+console.log(JsUser.FullName)
+console.log(JsUser[mySym])
+
+JsUser.email="sanjanashah0148@gmail.com"
+//Object.freeze(JsUser)//now changes will not propagate
+
+//function are treated as variables not differently
+
+JsUser.greeting=function(){
+    console.log("Hello JS User");
+}
+
+JsUser.greetingTwo=function(){
+    console.log(`Hello JS user ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
