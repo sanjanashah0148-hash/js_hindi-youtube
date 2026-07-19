e1fa0c78db0022f4e1c8c5388dd751171e72d239
sanjana shah <sanjanashah0148@gmail.com>
2026-07-19 06:09:53 +0000

    revision till lec4

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,7 +3,7 @@ let accountEmail="sanjanashah0148@gmail.com"
 var accountPassword="12345"
 accountCity="jaipur"
 let accountstate;
-// accountId=2 //not allowed
+// accountId=2 //not allowed-we cant change something with const keyword
 
 accountEmail="hdc@gmail.com"
 accountPassword="1234567"
@@ -17,3 +17,5 @@ becoz of issue in it block scope and functional scope
 console.log(accountId);
 
 console.table([accountId,accountEmail,accountPassword.accountCity,accountstate])
+//console.table to print everything in a table format at once rather than writing console.log everytime
+//js has problem with scope
