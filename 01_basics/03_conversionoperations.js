acd66f115fa4df5482215c36dee07dc37c8defd9
sanjana shah <sanjanashah0148@gmail.com>
2026-07-19 06:43:46 +0000

    revision lec6

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -1,15 +1,18 @@
-let score="33abc"
+let score="33abc"//data type:NaN
 console.log(typeof score);
 console.log(typeof (score));
 
 let valueInNumber=Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+//conversion
 //"33"=>33
-//"33abc"=>NaN
+//"33abc"=>NaN:type
 //truue=>1;false=>0
 
 let isLoggedIn="Sanjana"
+//similarly if empty string then the value is false
+//true ki 1 and false ki 0
 let booleanIsLoggedIn=Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn);
 
