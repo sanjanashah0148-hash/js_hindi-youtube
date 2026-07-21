6dec6bd0199b8ff71e23e2b8201c1ef1a2e6a202
sanjana shah <sanjanashah0148@gmail.com>
2026-07-21 12:12:23 +0000

    till lecture 30 done

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -8,3 +8,26 @@ const myTotal=myNums.reduce(function(acc,currval){
 },0)
 
 console.log(myTotal);
+
+const shoppingCart=[
+    {
+        itemName:"js course",
+        price:2999
+    },
+    {
+        itemName:"py course",
+        price:999
+    },
+    {
+        itemName:"mobile dev course",
+        price:5999
+    },
+    {
+        itemName:"data science course",
+        price:12999
+    },
+
+]
+
+const priceToPay=shoppingCart.reduce((acc,item)=>acc+item.price,0)//item har ek object of array
+console.log(priceToPay);
