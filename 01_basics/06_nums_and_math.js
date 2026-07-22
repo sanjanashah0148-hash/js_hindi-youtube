b4353683382c502480fb3940bbf0d703c18e1aa7
sanjana shah <sanjanashah0148@gmail.com>
2026-07-22 05:00:13 +0000

    revision of lec13

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,16 +1,18 @@
 const score=400
 console.log(score)
 
-const balance=new Number(100)
+const balance=new Number(100)//explicitly defining number as datatype
+//it specifically shows that it is a number
 console.log(balance);
 
-console.log(balance.toString().length);
+console.log(balance.toString().length);//changes into string
 console.log(balance.toFixed(2));//that means we are taking two precison values
 
 
 const otherNumber=23.8966
 
-console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toPrecision(3));//it return string
+//number can have value from 1 to 21
 
 const hundreds =1000000
 console.log(hundreds.toLocaleString());//according to US standard
@@ -20,15 +22,15 @@ console.log(hundreds.toLocaleString("en-IN"));//according to indian standards
 //+++++++++++++++Maths++++++++++++++++++++++++++++
 
 console.log(Math);
-console.log(Math.abs(-4));
+console.log(Math.abs(-4));//for absolute value
 
 console.log(Math.round(4.6));
 console.log(Math.ceil(4.5));
 console.log(Math.floor(4.5));
-console.log(Math.min(4,3,6,8));
+console.log(Math.min(4,3,6,8));//to find minimum value in an array
 console.log(Math.max(4,3,6,8));
 
-console.log(Math.random());//value of it is always less than 1
+console.log(Math.random());//value of it is always batween 0 and 1
 console.log((Math.floor(Math.random()*10)+1));//value of it is multiplies to 10 to get greater than 1 and added 1 for clarity
 
 const min=10
