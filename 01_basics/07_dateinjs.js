19557ddbdb0024999dcc63aee56450a3ae01b5ec
sanjana shah <sanjanashah0148@gmail.com>
2026-07-22 05:07:00 +0000

    revision lec14

--- 01_basics/07_dateinjs.js
+++ 01_basics/07_dateinjs.js
@@ -1,15 +1,16 @@
 //dates
 
-let myDate = new Date();
+let myDate = new Date();//new object of date
 
+console.log(myDate);//not readable
 console.log(myDate.toString());
 console.log(myDate.toDateString());
 console.log(myDate.toISOString());
 console.log(myDate.toJSON());
 console.log(myDate.toLocaleDateString());
-console.log(typeof myDate);
+console.log(typeof myDate);//it is an object
 
-let myCreatedDate = new Date("2026-07-09");
+let myCreatedDate = new Date("2026-07-09");//month start with zero in Js
 console.log(myCreatedDate.toDateString());
 
 let myTimeStamp = Date.now();
@@ -22,7 +23,7 @@ console.log(Math.floor(Date.now()/1000));
 let newDate=new Date()
 console.log(newDate);
 console.log(newDate.getMonth());
-console.log(newDate.getDay());
+console.log(newDate.getDay());//1 is monday
 
 
 newDate.toLocaleDateString('default',{
