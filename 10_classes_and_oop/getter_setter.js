99629c310c28551083ead3b5120fc924ecbfff14
sanjana shah <sanjanashah0148@gmail.com>
2026-07-25 08:11:00 +0000

    getter or setter

---
+++ 10_classes_and_oop/getter_setter.js
@@ -0,0 +1,10 @@
+class User{
+    constructor(email,password){
+        this.email=email;
+        this.password=password
+    }
+}
+
+const Sanjana=new User("sanjana@gmail.com","123")
+console.log(Sanjana.password);
+
