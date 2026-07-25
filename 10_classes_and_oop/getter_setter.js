9156baf9e84c774b880e74a8a33d5fa707b4fd6d
sanjana shah <sanjanashah0148@gmail.com>
2026-07-25 11:10:49 +0000

    getter and setter

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -3,15 +3,21 @@ class User{
         this.email=email;
         this.password=password
     }
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email=value
+    }
     get password(){
         return this.password.toUpperCase()
     }
-}
+    
     set password(value){
         this.password=value.toUpperCase()
     }
 }
 
-const Sanjana=new User("sanjana@gmail.com","123")
+const Sanjana=new User("sanjana@gmail.com","abc")
 console.log(Sanjana.password);
 
